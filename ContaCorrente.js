be2003ffd2597bfc6dea0d5d349837c2caf0ad53
Wagner Moreira <wagnermor@outlook.com>
2020-08-31 23:51:52 -0300

    Updated 'Cliente.js, ContaCorrente.js e index.js'

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,51 +1,15 @@
-import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class ContaCorrente {
+export class ContaCorrente extends Conta{
     static numeroDeContas = 0;
-    agencia;
-    
-    _cliente;
-    // #saldo = 0 https://github.com/tc39/proposital-class-fields#private-fields
-    _saldo = 0; 
-    
-    set cliente(novoValor) {
-        if(novoValor instanceof Cliente) {
-            this._cliente = novoValor;
-        }
-    }
-
-    get cliente() {
-        return this._cliente;
-    }
-
-    get saldo() {
-        return this._saldo;
-    }
-
     constructor(cliente, agencia) {
-        this.cliente = cliente;//Como _cliente já foi instanciado, não precisa utilizar o atributo privado "_cliente".
-        this.agencia = agencia;
+        super(0, cliente, agencia);
         ContaCorrente.numeroDeContas += 1;
     }
-    
-
+    // O método está sobrescrevendo o comportamento de sacar
     sacar(valor) {
-        if(this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
-    }
 
-    depositar(valor) {
-        if(this.valor <= 0) {
-            return;
-        }
-        this._saldo += valor;
-    }
-
-    transferir(valor, conta) {
-
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
+        let taxa = 1.1;
+        return this._sacar(valor, taxa);
     }
 }
--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Conta } from "./Conta.js";
 
 //Cadastro de clientes ==>
-const cliente001 = new Cliente("Coronilson Quarentenilson", 12345678923);
-const cliente002 = new Cliente("Quarentenilson Doismilevintino", 32165498774);
-const cliente003 = new Cliente("Covidelson Chinguelinguilson", 98745632114);
+const cliente001 = new Cliente("Covidelson Doismilevintino", 12345678923);
 
 //Cadastro de contas correntes ==>
-const conta003 = new ContaCorrente(cliente003, 2020);
-const conta002 = new ContaCorrente(cliente002, 2019);
-const conta001 = new ContaCorrente(cliente001, 114)
+const conta001C = new ContaCorrente(0, cliente001, 114)
+
+//Cadastro de conta poupança
+const conta001P = new ContaPoupanca(0, cliente001, 114);
+
+const conta = new Conta(0, "Quarentenilson Chinguiloidino", 171);
 
 //Movimentações das contas ==>
-conta003.depositar(500);
-conta003.sacar(50);
-conta003.transferir(100, conta001);
-conta003.transferir(200, conta002);
+conta001C.depositar(1000);
+conta001C.sacar(100);
+
+conta001P.depositar(1000);
+conta001P.sacar(100);
 
 //Impressões ==>
-console.log(conta002);
 console.log("\n");
-console.log(conta003);
+console.log(conta001C);
 console.log("\n");
-console.log(conta001);
+console.log(conta001P);
+console.log("\n");
+// console.log(conta);
+
 console.log(`\n Número de contas cadastradas: ${ContaCorrente.numeroDeContas}`);
 
-// console.log(`${JSON.stringify(conta003, null, 50)}`);
+// console.log(`${JSON.stringify(conta001, null, 50)}`);
